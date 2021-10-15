fdd1db1bae7016da542455278195bf85154805d9
Dillon Coleman <coleman399@gmail.com>
2021-10-15 11:31:03 -0700

    created classes for complete project

---
+++ computer.py
@@ -0,0 +1,6 @@
+from player import Player
+
+class Computer(Player):
+    def __init__(self):
+        self.name = "Computer"
+        super().__init__()
---
+++ human.py
@@ -0,0 +1,7 @@
+from player import Player
+
+
+class Human(Player):
+    def __init__(self, player):
+        self.name = player
+        super().__init__()
